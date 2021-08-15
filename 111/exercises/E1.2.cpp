903f58a7b875ef6ae60a545d6eaf544c46e8f8fb
routepoison <79955377+routepoison@users.noreply.github.com>
2021-08-14 23:03:49 -0400

    COMMIT
    COMMIT

---
+++ 111/exercises/E1.2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+// E 1.2.1
+
+cout << "# --- [ Exercise 1.2.1 ] --- #\n\n";
+
+cout << "I am Blaxxon, ";
+cout << "the godlike computer. ";
+cout << "Fear me!" << endl;
+
+// E 1.2.2
+
+cout << "# --- [ Exercise 1.2.2 ] --- #\n\n"
+
+cout << "I am Blaxxon,\n\n";
+cout << "the godlike computer.\n\n ";
+cout << "Fear me!" << endl;
+
+
+// E 1.2.1
+
+return 0;
+
+}
---
+++ 111/test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+
+for(int x = 0; x <=10; x++){
+    string wrd1 = "Word";
+    string wrd2 = "\n'";
+    cout << wrd1 << endl;
+    for (int y = 0; y <= 10; y++){
+        string str1 = "*";
+    }
+}
+
+return 0;
+}
