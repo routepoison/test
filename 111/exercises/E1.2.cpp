5b3f037a5028aa5e4185a85b12c84151a9303fe8
routepoison <79955377+routepoison@users.noreply.github.com>
2021-08-14 23:10:36 -0400

    COMMIT
    COMMIT

--- 111/exercises/E1.2.cpp
+++ 111/exercises/E1.2.cpp
@@ -14,14 +14,19 @@ cout << "Fear me!" << endl;
 
 // E 1.2.2
 
-cout << "# --- [ Exercise 1.2.2 ] --- #\n\n";
+cout << "\n# --- [ Exercise 1.2.2 ] --- #\n\n";
+
+cout << "I am Blaxxon,  ";
+cout << "the godlike computer.  ";
+cout << "Fear me!" << endl;
+
+// E 1.2.3 - Alter the example so that it prints two blank lines between each of the lines of output.
+
+cout << "\n# --- [ Exercise 1.2.3 ] --- #\n\n";
 
 cout << "I am Blaxxon,\n\n";
 cout << "the godlike computer.\n\n ";
 cout << "Fear me!" << endl;
-
-
-// E 1.2.1
 
 return 0;
 
